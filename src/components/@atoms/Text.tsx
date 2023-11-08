39ef8d7d8925d0c77f3d111468790f67376b07a8
jeewonMoon <mjw9912@gmail.com>
2023-11-08 17:59:01 +0900

    feat: text 컴포넌트 제작
    타입과 색상을 지정해서 사용
    색상 디폴트: 'black'

---
+++ src/components/@atoms/Text.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import { getColor } from '../../utils/colorUtils';
+import { theme } from '../../styles/theme/theme';
+
+type TextType = 'headline1' | 'headline2' | 'title1' | 'subtitle1' | 'body1';
+
+interface TextProps {
+  type?: TextType;
+  color?: keyof typeof theme.colors;
+}
+
+const textStyles: Record<
+  TextType,
+  { fontSize: string; fontWeight: string; lineHeight: string }
+> = {
+  headline1: {
+    fontSize: '16px',
+    fontWeight: '600',
+    lineHeight: '19.36px'
+  },
+  headline2: {
+    fontSize: '16px',
+    fontWeight: '400',
+    lineHeight: '18px'
+  },
+  title1: {
+    fontSize: '14px',
+    fontWeight: '400',
+    lineHeight: '18px'
+  },
+  subtitle1: {
+    fontSize: '12px',
+    fontWeight: '400',
+    lineHeight: '14.52px'
+  },
+  body1: {
+    fontSize: '11px',
+    fontWeight: '400',
+    lineHeight: '13.31px'
+  }
+};
+
+const StyledText = styled.div<TextProps>`
+  font-size: ${(props) =>
+    props.type ? textStyles[props.type].fontSize : '16px'};
+  font-weight: ${(props) =>
+    props.type ? textStyles[props.type].fontWeight : 'normal'};
+  line-height: ${(props) =>
+    props.type ? textStyles[props.type].lineHeight : 'normal'};
+  color: ${(props) =>
+    props.color ? getColor(props.color) : props.theme.colors.black};
+`;
+
+const TextComponent: React.FC<React.PropsWithChildren<TextProps>> = ({
+  children,
+  ...props
+}) => {
+  return <StyledText {...props}>{children}</StyledText>;
+};
+
+export default TextComponent;
