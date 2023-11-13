fdba364dbab6ef4afa209fae1584fdf386eaeb0f
최용호 <yongho6800@gmail.com>
2023-11-13 19:20:41 +0900

    feat/공지사항 페이지 및 텍스트 토글 컴포넌트 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -97,8 +97,6 @@ const Home: React.FC = () => {
         <gs.MainBox>
           <hs.HomeHeader>
             <hs.HeaderTextCtnr>
-              {/* ONCLICK */}
-              <Button type='secondary'>ss</Button>
               <Text type='headline1'>마음에 맞는 사람들과</Text>
               <Text type='headline2' color='neutral5'>
                 마음에 맞는 사람들과
