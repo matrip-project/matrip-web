ae94c5b6403ead5e2c07ab90d726077238dee8c2
yoonjoowon <wndnjs6224@naver.com>
2023-11-18 17:20:18 +0900

    feat : main 페이지 퍼블리싱 완료

--- src/pages/Home/homeStyle.tsx
+++ src/pages/Home/homeStyle.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 export const HomeHeader = styled.div`
@@ -35,3 +36,81 @@ export const tapTitle2 = styled.div`
   ${(props) => props.theme.texts.tapTitle2};
   margin-bottom: 12px;
 `;
+
+export const buttonWrraperContainer = styled.div`
+  position: relative;
+  width: 390px;
+`;
+
+export const postContainer = styled(Link)`
+  position: fixed;
+  bottom: 80px;
+  right: 25px;
+`;
+
+export const WriteBtn = styled.img``;
+
+export const ScheduleMoreBtn = styled(Link)`
+  display: inline-flex;
+  padding: 6px 14px 6px 16px;
+  align-items: flex-start;
+  gap: 10px;
+  border-radius: 48px;
+  border: 0.5px solid var(--Deactivation, #adadad);
+  background: #fff;
+  margin-bottom: 40px;
+
+  color: var(--sub, #666);
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 12px;
+`;
+
+export const PopularTravel = styled.div`
+  ${(props) => props.theme.texts.mainTitle};
+  width: 100%;
+  text-align: left;
+  margin-left: 23px;
+  margin-bottom: 20px;
+`;
+
+export const PopularTravelBox = styled.div`
+  display: flex;
+  margin-left: 23px;
+`;
+
+export const PopularImgbox = styled.div`
+  color: #000;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 22px;
+  text-align: center;
+  margin-right: 20px;
+`;
+
+export const PopularImg = styled.img``;
+
+export const InviteFriend = styled.button`
+  display: inline-flex;
+  padding: 12px 20px;
+  align-items: flex-start;
+  gap: 6px;
+  border-radius: 50px;
+  border: 0px;
+  background: var(--Main-color, #d30065);
+  color: white;
+  margin-top: 40px;
+  margin-bottom: 118px;
+
+  color: var(--theme-white-theme-core-tokens-pure-white, #fff);
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 12px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InviteFriendImg = styled.img``;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,11 @@ import React, { useState, useRef, useEffect } from 'react';
 import * as gs from '../../styles/GlobalStyles';
 import * as hs from './homeStyle';
 import logo from '../../asset/logo.png';
+import addPostButton from '../../asset/addPostButton.png';
+import jejuImg from '../../asset/ImgJeju.png';
+import busanImg from '../../asset/ImgBusan.png';
+import gangwonImg from '../../asset/ImgGangwon.png';
+import shareImg from '../../asset/share.png';
 
 import UserList from '../../components/UserList';
 import Search from '../../components/Search';
@@ -13,14 +18,41 @@ const Home: React.FC = () => {
         <hs.HomeHeader>
           <hs.HeaderLogo src={logo}></hs.HeaderLogo>
         </hs.HomeHeader>
-
-        <Search></Search>
-
+        <Search />
         <hs.TitleBox>
           <hs.MainTitle>동행일정</hs.MainTitle>
           <hs.tapTitle2>· 217개 동행일정을 둘러보세요.</hs.tapTitle2>
         </hs.TitleBox>
-        <UserList></UserList>
+        <UserList />
+        <hs.ScheduleMoreBtn to={'/ItineraryInfo'}>
+          일정 더보기 +
+        </hs.ScheduleMoreBtn>
+
+        <hs.PopularTravel>인기 여행지 TOP3</hs.PopularTravel>
+        <hs.PopularTravelBox>
+          <hs.PopularImgbox>
+            <hs.PopularImg src={jejuImg} />
+            제주
+          </hs.PopularImgbox>
+          <hs.PopularImgbox>
+            <hs.PopularImg src={busanImg} />
+            부산
+          </hs.PopularImgbox>
+          <hs.PopularImgbox>
+            <hs.PopularImg src={gangwonImg} />
+            강원
+          </hs.PopularImgbox>
+        </hs.PopularTravelBox>
+
+        <hs.InviteFriend>
+          친구 초대하기 <hs.InviteFriendImg src={shareImg}/>
+        </hs.InviteFriend>
+
+        <hs.buttonWrraperContainer>
+          <hs.postContainer to={'/ItineraryInfo'}>
+            <hs.WriteBtn src={addPostButton}></hs.WriteBtn>
+          </hs.postContainer>
+        </hs.buttonWrraperContainer>
       </gs.MainContainer>
     </>
   );
