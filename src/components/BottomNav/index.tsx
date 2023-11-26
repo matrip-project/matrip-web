f5738beed6cf622b5b391cb0b2e9f476e730e956
yoonjoowon <wndnjs6224@naver.com>
2023-11-26 12:15:47 +0900

    feat : 하단 nav bar 기능에 맞게 수정

--- src/components/BottomNav/index.tsx
+++ src/components/BottomNav/index.tsx
@@ -7,31 +7,87 @@ import { BsPerson, BsGrid } from 'react-icons/bs';
 import { getColor } from '../../utils/colorUtils';
 import { useDispatch } from 'react-redux';
 import { setKeyword } from '../../redux/modules/keywordImgSlice';
+import navHomeIcon from '../../asset/navHomeIcon.svg';
+import navAccompanyIcon from '../../asset/navAccompanyIcon.svg';
+import navPopularIcon from '../../asset/navPopularIcon.svg';
+import navMupageIcon from '../../asset/navMupageIcon.svg';
+import navNoneHome from '../../asset/navNoneHome.svg';
+import navNoneAccompany from '../../asset/navNoneAccompany.svg';
+import navNonePopular from '../../asset/navNonePopular.svg';
+import navNoneMypage from '../../asset/navNoneMypage.svg';
 
 function BottomNav() {
-  const [isActive, setIsActive] = useState<number>(1);
+  const [activeIcons, setActiveIcons] = useState<number[]>([1, 0, 0, 0]);
   const dispatch = useDispatch();
+
+  const getIconImage = (index: number) => {
+    if (activeIcons[index - 1]) {
+      switch (index) {
+        case 1:
+          return navHomeIcon;
+        case 2:
+          return navAccompanyIcon;
+        case 3:
+          return navPopularIcon;
+        case 4:
+          return navMupageIcon;
+        default:
+          return '';
+      }
+    } else {
+      switch (index) {
+        case 1:
+          return navNoneHome;
+        case 2:
+          return navNoneAccompany;
+        case 3:
+          return navNonePopular;
+        case 4:
+          return navNoneMypage;
+        default:
+          return '';
+      }
+    }
+  };
+
+  const handleIconClick = (index: number) => {
+    setActiveIcons((prev) => prev.map((_, i) => (i === index - 1 ? 1 : 0)));
+
+    if (index === 3) {
+      dispatch(setKeyword('제주'));
+    }
+  };
 
   return (
     <NavContainer>
       <NavWrap>
-        <NavBtn to={'/'} onClick={() => setIsActive(1)}>
-          <StyledHome size='30' $isActive={isActive} />
+        <NavBtn onClick={() => handleIconClick(1)} to={'/'}>
+          <StyledIcon
+            src={getIconImage(1)}
+            isActive={activeIcons[0] === 1}
+            alt='홈 아이콘'
+          />
         </NavBtn>
-        <NavBtn to={'/CompanionList'} onClick={() => setIsActive(2)}>
-          <StyledGlass size='30' $isActive={isActive} />
+        <NavBtn onClick={() => handleIconClick(2)} to={'/companionList'}>
+          <StyledIcon
+            src={getIconImage(2)}
+            isActive={activeIcons[1] === 1}
+            alt='동행 아이콘'
+          />
         </NavBtn>
-        <NavBtn
-          to={'/PopularTravel'}
-          onClick={() => {
-            setIsActive(3);
-            dispatch(setKeyword('제주'));
-          }}
-        >
-          <StyledGrid size='30' $isActive={isActive} />
+        <NavBtn onClick={() => handleIconClick(3)} to={'/PopularTravel'}>
+          <StyledIcon
+            src={getIconImage(3)}
+            isActive={activeIcons[2] === 1}
+            alt='인기 여행 아이콘'
+          />
         </NavBtn>
-        <NavBtn to={'/userProfile'} onClick={() => setIsActive(4)}>
-          <StyledPerson size='30' $isActive={isActive} />
+        <NavBtn onClick={() => handleIconClick(4)} to={'/PopularTravel'}>
+          <StyledIcon
+            src={getIconImage(4)}
+            isActive={activeIcons[3] === 1}
+            alt='마이페이지 아이콘'
+          />
         </NavBtn>
       </NavWrap>
     </NavContainer>
@@ -65,21 +121,6 @@ const NavBtn = styled(Link)`
   padding: 0 15px;
 `;
 
-const StyledHome = styled(BiHomeAlt)<{ $isActive: number }>`
-  color: ${({ $isActive }) =>
-    $isActive === 1 ? getColor('primary') : getColor('black')};
-`;
-const StyledGlass = styled(HiMiniMagnifyingGlass)<{ $isActive: number }>`
-  color: ${({ $isActive }) =>
-    $isActive === 2 ? getColor('primary') : getColor('black')};
-`;
-const StyledGrid = styled(BsGrid)<{ $isActive: number }>`
-  color: ${({ $isActive }) =>
-    $isActive === 3 ? getColor('primary') : getColor('black')};
-`;
-const StyledPerson = styled(BsPerson)<{ $isActive: number }>`
-  color: ${({ $isActive }) =>
-    $isActive === 4 ? getColor('primary') : getColor('black')};
-`;
+const StyledIcon = styled.img<{ isActive: boolean }>``;
 
 export default BottomNav;
