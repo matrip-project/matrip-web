a6e0d42be83c474841d2d8a976243309076c7a70
윤주원 <118035287+YoonJoowon@users.noreply.github.com>
2023-11-27 22:17:53 +0900

    MT-91 날짜 기능 구현  (#26)
    * fix : png 이미지 파일 svg로 교체
    
    * feat : 인기여행페이지 img 세팅
    
    * feat : 인기 페이지 라우팅 기능 추가
    
    * feat : 인기여행지 클릭시 해당 키워드 검색기능 구현
    
    * feat : 게시글 없을 때 텍스트 문구 기능 추가
    
    * feat : 인기페이지 세팅값이 없을 때 가장 인기있는 여행지 제주로 세팅되게 수정
    
    * fix : 필터아이콘 및 타이틀 텍스트 박스 필터링 배열에 따라 비전 여부 버그 수정
    
    * fix :
    
    * feat : 필터 아이콘 및 드롭다운 기능 구현
    
    * feat : 하단 nav bar 기능에 맞게 수정
    
    * feat : nav bar 상단 색 줄 변경
    
    * fix : nav style 수정
    
    * feat : top nav 작성 및 라우팅 구현
    
    * fix : top nav 도출 페이지 버그 수정
    
    * faet : top nav 뒤로가기 버튼 구현
    
    * feat : 날짜 기능 개발

--- src/components/SelectButton/index.tsx
+++ src/components/SelectButton/index.tsx
@@ -1,6 +1,9 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
+import { addDays } from 'date-fns';
 import Select, { StylesConfig } from 'react-select';
 import styled from 'styled-components';
+import { DateRange, DateRangePicker } from 'react-date-range';
+import dropdownIcon from '../../asset/dropdownIcon.svg';
 
 const genderOptions = [
   { value: '남성', label: '남성' },
@@ -22,6 +25,34 @@ const Recruitment = [
 ];
 
 const SelectButton: React.FC = () => {
+  // date
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
+  const [dateRange, setDateRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 1),
+      key: 'selection'
+    }
+  ]);
+
+  const handleDateBoxBtnClick = () => {
+    setDatePickerVisible(!datePickerVisible);
+  };
+
+  const handleRangeChange = (ranges: any) => {
+    setDateRange([ranges.selection]);
+  };
+
+  const formatSelectedDate = () => {
+    const { startDate, endDate } = dateRange[0];
+    if (startDate && endDate) {
+      const formattedStartDate = startDate.toLocaleDateString();
+      const formattedEndDate = endDate.toLocaleDateString();
+      return `${formattedStartDate} ~ ${formattedEndDate}`;
+    }
+    return '날짜';
+  };
+
   const customStyles = useMemo<StylesConfig>(
     () => ({
       option: (provided, state) => ({
@@ -32,7 +63,6 @@ const SelectButton: React.FC = () => {
       }),
       control: (provided, state) => ({
         ...provided
-        // borderColor: state.isFocused ? '#D30065' : 'grey'
       }),
       singleValue: (provided, state) => ({
         ...provided
@@ -42,32 +72,46 @@ const SelectButton: React.FC = () => {
   );
 
   return (
-    <SelectBox>
-      <Select
-        options={genderOptions}
-        placeholder='성별'
-        styles={customStyles}
-        components={{ IndicatorSeparator: () => null }}
-      />
-      <Select
-        options={age}
-        placeholder='나이'
-        styles={customStyles}
-        components={{ IndicatorSeparator: () => null }}
-      />
-      <Select
-        options={genderOptions}
-        placeholder='날짜'
-        styles={customStyles}
-        components={{ IndicatorSeparator: () => null }}
-      />
-      <Select
-        options={Recruitment}
-        placeholder='모집상태'
-        styles={customStyles}
-        components={{ IndicatorSeparator: () => null }}
-      />
-    </SelectBox>
+    <>
+      <SelectBox>
+        <Select
+          options={genderOptions}
+          placeholder='성별'
+          styles={customStyles}
+          components={{ IndicatorSeparator: () => null }}
+        />
+        <Select
+          options={age}
+          placeholder='나이'
+          styles={customStyles}
+          components={{ IndicatorSeparator: () => null }}
+        />
+
+        <DateComponent onClick={handleDateBoxBtnClick}>
+          <DateComponentText>
+            날짜 <img src={dropdownIcon} />
+          </DateComponentText>
+          <DateComponentcalendar></DateComponentcalendar>
+        </DateComponent>
+
+        <Select
+          options={Recruitment}
+          placeholder='모집상태'
+          styles={customStyles}
+          components={{ IndicatorSeparator: () => null }}
+        />
+      </SelectBox>
+      {datePickerVisible && (
+        <DateRange
+          editableDateInputs={true}
+          onChange={handleRangeChange}
+          moveRangeOnFirstSelection={false}
+          ranges={dateRange}
+          months={1}
+          direction='horizontal'
+        />
+      )}
+    </>
   );
 };
 
@@ -81,4 +125,26 @@ const SelectBox = styled.div`
   color: gray;
   margin-bottom: 16px;
   padding: 0px 20px 0px 10px;
+`;
+
+const DateComponent = styled.div`
+  border-radius: 5px 5px 0px 0px;
+  border: 1px solid var(--box-stroke, #d9d9d9);
+  background: #fff;
+  width: 60px;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  display: flex;
+  position: relative;
+`;
+
+const DateComponentText = styled.div`
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+`;
+
+const DateComponentcalendar = styled.div`
+  top: 200px;
 `;
