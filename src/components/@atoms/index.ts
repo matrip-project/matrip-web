a891ce46095cc1a7aa5f8e9a36a6aa3c032f1401
yoonjoowon <wndnjs6224@naver.com>
2023-11-27 18:18:11 +0900

    faet : top nav 뒤로가기 버튼 구현

--- src/components/@atoms/index.ts
+++ src/components/@atoms/index.ts
@@ -3,10 +3,11 @@ import CheckBox from './CheckBox';
 import Collapsible from './Collapsible';
 import Image from './Image';
 import InputLabel from './InputLabel';
-import InterestButton from './InterestButton';
+// import InterestButton from './InterestButton';
 import Spacer from './Spacer';
 import TabMenu from './TabMenu';
 import Text from './Text';
 
 
-export {Button, CheckBox, Collapsible, Image, InputLabel, InterestButton, Spacer, TabMenu, Text};
+export {Button, CheckBox, Collapsible, Image, InputLabel, Spacer, TabMenu, Text};
+// InterestButton
