c0d4dd441f19c8054a3b512634a7be4d403d433d
문지원 <81554184+jeewonMoon@users.noreply.github.com>
2023-12-08 14:51:48 +0900

    MT-97 위치 설정 기능 구현
    - 리덕스 툴킷을 이용해 위도와 경도를 저장하도록 했습니다.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import searchReducer from './modules/searchSlice';
 import dibsSlice from './modules/dibSlice';
 import keywordImgSlice from './modules/keywordImgSlice';
+import locationSlice from './modules/locationSlice';
 
 export const store = configureStore({
   reducer: {
     search: searchReducer,
     dibsSlice: dibsSlice,
-    keyword: keywordImgSlice
+    keyword: keywordImgSlice,
+    location: locationSlice
   }
 });
 
