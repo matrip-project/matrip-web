e59b9319a95e5aeb69d51c6eba146ddbf9c3e15b
문지원 <81554184+jeewonMoon@users.noreply.github.com>
2024-01-04 02:21:10 +0900

    design: 인원 조정 버튼 추가 (#71)
    - 글 작성 페이지에 인원 수 조정하는 버튼을 추가했습니다.

--- src/pages/Posting/postingComponents/PersonnelInput.tsx
+++ src/pages/Posting/postingComponents/PersonnelInput.tsx
@@ -1,18 +1,50 @@
+import { useState } from 'react';
 import styled from 'styled-components';
-import { PostingContainer, StateProps } from '..';
-import Label from './Label';
 import { useDispatch } from 'react-redux';
 import { setData } from '../../../redux/modules/postSlice';
+import { PostingContainer, StateProps } from '..';
+import Label from './Label';
+import { ReactComponent as Up } from '../../../asset/up.svg';
 
 function PersonnelInput({ dataInput }: StateProps) {
   const dispatch = useDispatch();
+  const [value, setValue] = useState(0);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const parsedValue = parseInt(e.target.value);
+
+    setValue(parsedValue);
+
     if (dataInput) {
       dispatch(
         setData({
           ...dataInput,
-          count: parseInt(e.target.value)
+          count: parsedValue
+        })
+      );
+    }
+  };
+
+  const increment = () => {
+    setValue((value) => value + 1);
+    if (dataInput) {
+      dispatch(
+        setData({
+          ...dataInput,
+          count: value + 1
+        })
+      );
+    }
+  };
+
+  const decrement = () => {
+    setValue((value) => value - 1);
+    if (dataInput) {
+      dispatch(
+        setData({
+          ...dataInput,
+          count: value - 1
         })
       );
     }
@@ -21,12 +53,11 @@ function PersonnelInput({ dataInput }: StateProps) {
   return (
     <PostingContainer>
       <Label label='동행 모집 인원' essential={true} />
-      <InputWrap
-        type='number'
-        min={0}
-        onChange={handleChange}
-        value={dataInput?.count}
-      />
+      <InputWrap type='number' min={0} onInput={handleInput} value={value} />
+      <ButtonWrap>
+        <Up onClick={increment} />
+        <Down onClick={decrement} />
+      </ButtonWrap>
     </PostingContainer>
   );
 }
@@ -36,6 +67,21 @@ const InputWrap = styled.input`
   height: 36px;
   padding-left: 7px;
   border: 1px solid ${(props) => props.theme.colors.neutral1};
+  position: relative;
+`;
+
+const ButtonWrap = styled.div`
+  position: absolute;
+  display: flex;
+  right: 8%;
+  top: 55.3%;
+  flex-direction: column;
+  gap: 10px;
+  cursor: pointer;
+`;
+
+const Down = styled(Up)`
+  transform: scaleY(-1);
 `;
 
 export default PersonnelInput;
